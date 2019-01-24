5481e8c28bccf15c8f38490cba2e6a6a463cb46a
david.huang <david.huang@getac.com.tw>
2019-01-24 17:28:41 +0800

    [Lec208]

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '753927733967-4kmf6hoq66hoq1r7ctcirlbgck9aj9rk.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
