c7a7bf4b5fb90c911ab7614632a2d770272e7fcf
david.huang <david.huang@getac.com.tw>
2019-01-25 16:17:59 +0800

    [Lec213]

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -9,19 +9,42 @@ class GoogleAuth extends React.Component {
         clientId: '753927733967-4kmf6hoq66hoq1r7ctcirlbgck9aj9rk.apps.googleusercontent.com',
         scope: 'email'
       }).then(() => {
-        this.auth = window.gapi.auth2.getAythInstance();
+        this.auth = window.gapi.auth2.getAuthInstance();
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignInClicked = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClicked = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I dont know if we are signed in</div>
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in!</div>
+      return (
+        <button onClick={this.onSignOutClicked} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      )
     } else {
-      return <div>i am not signed in</div>
+      return (
+        <button onClick={this.onSignInClicked} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
 
