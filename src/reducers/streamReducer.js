733a0c6c3fd9811031bd2df76cdfc4bbd3a9c68f
david.huang <david.huang@getac.com.tw>
2019-01-29 13:14:05 +0800

    [Lec 244]

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+import {
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM
+} from '../actions/action-list';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case
+      FETCH_STREAM,
+      CREATE_STREAM,
+      EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+    default:
+      return state;
+  }
+};
