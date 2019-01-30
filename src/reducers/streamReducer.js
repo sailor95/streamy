9f7e1b0fb278b76c171b0a68f49c11ed5f6fa815
david.huang <david.huang@getac.com.tw>
2019-01-30 14:00:46 +0800

    [Lec 247]

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -11,10 +11,9 @@ export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAMS:
       return { ...state, ..._.mapKeys(action.payload, 'id') };
-    case
-      FETCH_STREAM,
-      CREATE_STREAM,
-      EDIT_STREAM:
+    case FETCH_STREAM:
+    case CREATE_STREAM:
+    case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case DELETE_STREAM:
       return _.omit(state, action.payload);
