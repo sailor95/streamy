83ccd2b0ec3248c6040759e58f4d0947edccd9e3
david.huang <david.huang@getac.com.tw>
2019-03-04 15:30:13 +0800

    [Lec266] Init value of form

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 
 import { fetchStream } from '../../actions/streamyActions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
   render() {
-    console.log(this.props);
-    return <div>StreamEdit</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          onSubmit={this.onSubmit} />
+      </div>
+    );
   }
 }
 
